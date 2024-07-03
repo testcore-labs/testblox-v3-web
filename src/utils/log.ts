4d06b59920d93b5ab1ecd9dcd50746f3f401a973
qzip <qzip@tc-labs.win>
2024-07-03 19:17:10 +0300

    time util expansion pack for the nintendo 64

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,12 +1,11 @@
 import colors from 'colors/safe';
-import date_format, { masks } from "dateformat";
+import { log_time } from '../utils/time';
 
 class logs {
   static colors: typeof colors;
   static logs: { server: string[]; database: string[]; };
 
   static {
-    masks.log_time = 'ddd HH:MM:ss:L';
     logs.colors = colors;
     logs.logs = {
       server: [],
@@ -14,12 +13,12 @@ class logs {
     };
   }
   static server_log(txt: string | number) {
-    const message = colors.white("["+date_format(Date.now(), "log_time")+"]") + " " + colors.cyan("[server]") + ": " + txt;
+    const message = colors.white("["+log_time(Date.now())+"]") + " " + colors.cyan("[server]") + ": " + txt;
     logs.logs.server.push(message);
     console.log(message);
   }
   static database_log(txt: string | number) {
-    const message = colors.white("["+date_format(Date.now(), "log_time")+"]") + " " + colors.red("[database]") + ": " + txt;
+    const message = colors.white("["+log_time(Date.now())+"]") + " " + colors.red("[database]") + ": " + txt;
     logs.logs.database.push(message);
     console.log(message);
   }
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,3 +1,11 @@
-function rbx_time(date: Date) { // 1970-01-01T00:00:00Z
+import date_format, { masks } from "dateformat";
+masks.log_time = 'ddd HH:MM:ss:L';
+
+export function rbx_time(timestamp: number) { // 1970-01-01T00:00:00Z
+  let date = new Date(timestamp);
   return date.toISOString().split('.')[0] + 'Z';
+}
+
+export function log_time(timestamp: number) {
+  return date_format(timestamp, "log_time");
 }
