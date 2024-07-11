0ea3dbd1d8cbccdae0dcf165b53c6dd1e9ebf074
qzip <qzip@tc-labs.win>
2024-07-11 11:06:47 +0300

    purge script for logs

---
+++ src/cli/purge_logs.ts
@@ -0,0 +1,30 @@
+import colors from "colors";
+import readline from 'node:readline';
+import fs from 'fs';
+import path from 'path';
+import root_path from '../utils/root_path';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const dir = path.join(root_path, "logs");
+rl.question(`are you sure you want to purge `+ colors.red("ALL") +` logs? Y/N`, answer => {
+  let new_answer = answer.toLowerCase();
+  if(["yes", "ye", "y"].some(yes => new_answer == yes)) {
+    console.log("purging..");
+    fs.readdir(dir, (err, files) => {
+      if (err) throw err;
+    
+      for (const file of files) {
+        if(file.includes(".log")) {
+          fs.unlink(path.join(dir, file), (err) => {
+            if (err) throw err;
+          });
+        }
+      }
+    });    
+  }
+  rl.close();
+});
