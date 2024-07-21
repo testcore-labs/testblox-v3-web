ab7cc094be86153b91b52c453a28d310c8248aba
qzip <qzip@tc-labs.win>
2024-07-21 09:32:13 +0300

    more pagination, some fixes

--- src/utils/twig.ts
+++ src/utils/twig.ts
@@ -25,6 +25,18 @@ twig.extendFunction("transcompletion", function(locale: string): any {
   return translate.completion(locale);
 });
 
+twig.extendFunction("query_remove_key", function(query: {[key: string]: any}, remove: string) {
+  let parsed = { ...query };
+  delete parsed[remove];
+  
+  let str = "";
+  Object.entries(parsed).forEach(([key, value]) => {
+    str += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+  });
+  
+  return str;
+});
+
 twig.extendFunction("env", function(key: any): any {
   return env[key];
 });
